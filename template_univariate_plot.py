cd543d51a20c2cebb2b9895c268f17e683b515cf
dengyuwen <2112164129@e.gzhu.edu.cn>
2023-09-01 11:31:16 +0800

    update

--- template_univariate_plot.py
+++ template_univariate_plot.py
@@ -19,7 +19,7 @@ def univariate_histplot(data:DataFrame, x:str, hue=None, figsize=(10,6), style=[
     figsize: 图片尺寸，默认为(10, 6)。
     style: list, 画图的风格，默认为['science']，可以改为['science', 'ieee']
     kde: bool, 是否添加核密度估计曲线，默认为True.
-    kind: str, 图像类型，hist为直方图，kde为密度图。
+    kind: str, 图像类型，hist为直方图，kde为密度图，box为箱线图。
     plot_dpi: int, 画图时的dpi。
     saveflag: bool, 是否保存图片，默认为False。
     save_dir: str, 保存路径。
@@ -38,29 +38,39 @@ def univariate_histplot(data:DataFrame, x:str, hue=None, figsize=(10,6), style=[
         raise ValueError("Input 'hue' must be a column name in data")
     if not save_dir.endswith('/'):
         save_dir = save_dir + '/'
-    if kind not in ['hist', 'kde']:
-        raise ValueError("Input 'kind' must be 'hist' or 'kde'")
+    if kind not in ['hist', 'kde', 'box']:
+        raise ValueError("Input 'kind' must be 'hist' or 'kde' or 'box'.")
     
     with plt.style.context(style):
         fig, ax = plt.subplots(figsize=figsize, dpi=plot_dpi, facecolor="w")
-        if hue and kind == 'hist':
-            ax = sns.histplot(data=data, x=x, hue=hue, multiple='dodge', shrink=.8)
-        elif hue and kind == 'kde':
-            ax = sns.kdeplot(data=data, x=x, hue=hue, fill=True, alpha=.5)
+        if hue:
+            if kind == 'hist':
+                ax = sns.histplot(data=data, x=x, hue=hue, multiple='dodge', shrink=.8)
+            if kind == 'kde':
+                ax = sns.kdeplot(data=data, x=x, hue=hue, fill=True, alpha=.5)
+            if kind == 'box':
+                ax = sns.boxplot(data=data, x=x, y=hue)
         else:
-            ax = sns.histplot(data=data, x=x, kde=kde)
+            if kind == 'hist':
+                ax = sns.histplot(data=data, x=x, kde=kde)
+            if kind == 'kde':
+                ax = sns.kdeplot(data=data, x=x, fill=True, alpha=.5)
+            if kind == 'box':
+                ax = sns.boxplot(data=data, x=x)
 
         ax.set_xlabel('Values') # 画完图后不能再有参数出现
         if kind == 'hist':
             ax.set_ylabel('Frequency')  
+        elif kind == 'box':
+            pass
         else:
             ax.set_ylabel('Density')
 
         if saveflag:
             if hue:
-                save_path = save_dir + kind + x + '_' + hue + '_' + style[-1] + '.' + save_type
+                save_path = save_dir + kind + '_' + x + '_' + hue + '_' + style[-1] + '.' + save_type
             else:
-                save_path = save_dir + kind + x + '_' + style[-1] + '.' + save_type
+                save_path = save_dir + kind + '_' + x + '_' + style[-1] + '.' + save_type
             plt.savefig(save_path, dpi=save_dpi, bbox_inches='tight')
             print(f'Plot has been saved to {save_path} .')
 
@@ -68,5 +78,5 @@ def univariate_histplot(data:DataFrame, x:str, hue=None, figsize=(10,6), style=[
 
 if __name__ == "__main__":
     iris = sns.load_dataset('iris')
-    univariate_histplot(data=iris, x='sepal_length', hue='species', kind='kde')
+    univariate_histplot(data=iris, x='sepal_length', hue='species', kind='kde') # hue='species', 
     print('Done.')
